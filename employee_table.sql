d1ab3f61d363e3a8ffcc667ff0d364f4115bc4c4
Jeff Akins <jeffakins21@gmail.com>
2021-08-10 21:41:48 -0700

    Complete with database correlation tests

--- employee_table.sql
+++ employee_table.sql
@@ -5,9 +5,21 @@ JOIN salaries
 	-- WHERE to_date > NOW(); */
 	
 	
-select datediff(curdate(), hire_date) as days_with_company, salary
+/* select datediff(curdate(), hire_date) as days_with_company, salary
 from employees
 join salaries using(emp_no)
-where to_date > curdate();
+where to_date > curdate(); */
 
+SELECT *
+FROM titles;
+
+SELECT emp_no, hire_date, title
+FROM employees
+JOIN titles USING(emp_no);
+
+SELECT emp_no, 
+	datediff(curdate(), hire_date) AS days_with_company,
+	hire_date, title
+FROM employees
+JOIN titles USING(emp_no);
 
