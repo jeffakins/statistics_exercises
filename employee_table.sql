ab5ba9c6103c1c6146353fc6b67ba9bcb813d2c3
Jeff Akins <jeffakins21@gmail.com>
2021-08-11 14:32:00 -0700

    Complete with chi2 exercises

--- employee_table.sql
+++ employee_table.sql
@@ -23,3 +23,40 @@ SELECT emp_no,
 FROM employees
 JOIN titles USING(emp_no);
 
+
+SELECT emp_no, count(title) AS title_count, datediff(curdate(), hire_date) AS days_on_job
+FROM employees
+JOIN titles USING(emp_no)
+GROUP BY emp_no;
+
+SELECT * 
+FROM employees;
+
+SELECT * 
+FROM departments;
+
+SELECT * 
+FROM dept_manager;
+
+SELECT emp_no, gender,
+	IF(dept_no LIKE 'd%', True, False) AS is_manager
+FROM employees
+LEFT JOIN dept_manager USING(emp_no);
+
+-- Is an employee's gender independent of whether an employee works in sales or marketing? 
+-- (only look at current employees)
+
+SELECT * 
+FROM departments;
+SELECT * 
+FROM dept_emp;
+SELECT * 
+FROM employees;
+
+SELECT emp_no, gender, dept_name
+FROM dept_emp 
+LEFT JOIN departments USING(dept_no)
+JOIN employees USING(emp_no)
+WHERE to_date > NOW() AND (dept_no = 'd001' OR dept_no = 'd007');
+
+
